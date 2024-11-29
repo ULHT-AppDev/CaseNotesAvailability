e94b207a347c5be90db438e83887114499a81c6f
Nidheesh Palakkal <Nidheesh.Palakkal@ULH.nhs.uk>
2024-11-29 15:52:28 +0000

    Create ImprovementDetailsCallbackBO.cs

---
+++ BusinessObjects/ImprovementDetailsCallbackBO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class ImprovementDetailsCallbackBO
+    {
+        public int RequiresImprovementDetailsID { get; set; }
+        public string Comment { get; set; }
+        public int ImprovementDetailID { get; set; }
+
+    }
+} 
